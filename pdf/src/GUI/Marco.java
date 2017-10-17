3c9420f267faafb1377112584c493af5e5c997cc
Asus <Asus@Asus-PC>
2017-10-17 18:45:02 -0500

    Creacion de metodo de panel,nombre de variables y importacion de clases

--- pdf/src/GUI/Marco.java
+++ pdf/src/GUI/Marco.java
@@ -1,20 +1,30 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package GUI;
 
+import javax.swing.JDesktopPane;
 import javax.swing.JFrame;
+import javax.swing.JInternalFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 
-/**
- *
- * @author Oswaldo Merino
- */
+
 public class Marco extends JFrame{
-  
+   private final JDesktopPane contenedor;
+    private Panel[] panel = new Panel[50];//Creacion del arreglo        
+    private int x = 0;//Variable que manejara el arreglo
+    private int pos;
+    private boolean v = false;
+    private final JMenuBar menu;
+    private final JMenu archivo;
+    private final JMenuItem nuevo;
+    private JInternalFrame internal;
+
     
     public Marco() {
+        
+    }
+    public void añadirFrameInterno(Panel p) {
         
     }
 }

d8bbca59f4db97c8facf761d48931eae40309e48
Asus <Asus@Asus-PC>
2017-10-17 18:53:44 -0500

    Creacion de clase panel,Agregacion de comentarios en Marco y Inicializando el JFrame de marco

--- pdf/src/GUI/Marco.java
+++ pdf/src/GUI/Marco.java
@@ -10,19 +10,29 @@ import javax.swing.JMenuItem;
 
 
 public class Marco extends JFrame{
-   private final JDesktopPane contenedor;
+   private final JDesktopPane contenedor; //Contenedor del frame interno
     private Panel[] panel = new Panel[50];//Creacion del arreglo        
-    private int x = 0;//Variable que manejara el arreglo
+    private int x = 0;                    //Variable que manejara el arreglo
     private int pos;
-    private boolean v = false;
+    private boolean v = false;     
     private final JMenuBar menu;
     private final JMenu archivo;
     private final JMenuItem nuevo;
-    private JInternalFrame internal;
+    private JInternalFrame internal;      //Frame Interno
 
     
     public Marco() {
+        contenedor = new JDesktopPane();
+        add(contenedor);
+        nuevo = new JMenuItem("Nuevo");//Menu para crear una ventana nueva
         
+        //Agregar clase accionListener
+        
+        archivo = new JMenu("Archivo");//Nombre de etiqueta
+        archivo.add(nuevo);
+        menu = new JMenuBar();
+        menu.add(archivo); //Agregar el archivo a la barra
+        setJMenuBar(menu);
     }
     public void añadirFrameInterno(Panel p) {
         

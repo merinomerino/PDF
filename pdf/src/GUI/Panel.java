093eb617a8b9e67a9e7581d168ea34f45edd4b37
merinomerino <32102477+merinomerino@users.noreply.github.com>
2017-10-18 16:31:02 -0500

    se añadio Frame Interno

--- pdf/src/GUI/Panel.java
+++ pdf/src/GUI/Panel.java
@@ -1,11 +1,8 @@
 package GUI;
 
-import java.awt.*;
-import java.awt.event.*;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import javax.swing.*;
+import javax.swing.JPanel;
+
+
 
 public class Panel extends JPanel {
 

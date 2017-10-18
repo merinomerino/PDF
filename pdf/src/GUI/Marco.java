d99df980a20dc25dc1ebf74c31d64df793d526a0
AngelAbraham <32169462+AngelAbraham@users.noreply.github.com>
2017-10-18 17:50:46 -0500

    Agregacion de accion listener en marco
    El codigo de marco esta completo

--- pdf/src/GUI/Marco.java
+++ pdf/src/GUI/Marco.java
@@ -1,6 +1,7 @@
-
 package GUI;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.JDesktopPane;
 import javax.swing.JFrame;
 import javax.swing.JInternalFrame;
@@ -27,6 +28,29 @@ public class Marco extends JFrame{
         nuevo = new JMenuItem("Nuevo");//Menu para crear una ventana nueva
         
         //Agregar clase accionListener
+       nuevo.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (pos > 0 || v == true) {
+                    internal = new JInternalFrame("Nuevo ", true, true, true,
+                            true);
+                    panel[pos] = new Panel();
+                    panel[pos].nuevo();
+                    internal.setName(String.valueOf(pos));
+                    añadirFrameInterno(panel[pos]);
+                    pos = 0;
+                    v = false;
+                } else if (panel[x] == null) {
+                    internal = new JInternalFrame("Nuevo ", true, true, true,
+                            true);
+                    panel[x] = new Panel();
+                    panel[x].nuevo();
+                    internal.setName(String.valueOf(x));
+                    añadirFrameInterno(panel[x]);
+                }
+                x++;
+            }
+        });
         
         archivo = new JMenu("Archivo");//Nombre de etiqueta
         archivo.add(nuevo);

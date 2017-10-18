3956479e46c04b0d8faa19347ececc0aa18ecbf3
AngelAbraham <32169462+AngelAbraham@users.noreply.github.com>
2017-10-18 18:13:50 -0500

    Creacion de variables y importacion de paquetes
    Se agregaron las variables a utilizar

--- pdf/src/GUI/Panel.java
+++ pdf/src/GUI/Panel.java
@@ -1,11 +1,26 @@
 package GUI;
 
-import javax.swing.JPanel;
-
+import Etiquetas.Etiquetas;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import javax.swing.*;
 
 
 public class Panel extends JPanel {
-
+    private final JPanel panel;
+    private JTextArea editor;
+    private JScrollPane scroll;
+    private final JMenuBar menu;
+    private final JMenu archivo;
+    private final JMenuItem save;
+    private final JMenuItem saveAs;
+    private final JMenuItem open;
+    private JFileChooser elegir;
+    private File ruta; 
+    private File rutaT;
     public Panel() {
 
     }

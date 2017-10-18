ccb71a7ac781e95c4a79fc4ca484bafbf291858a
DESKTOP-9VS7B9R\santo <diegohans96@gmail.com>
2017-10-18 18:46:55 -0500

    Panel
    Agregacion del menu al panel interno

--- pdf/src/GUI/Panel.java
+++ pdf/src/GUI/Panel.java
@@ -8,8 +8,8 @@ import java.io.File;
 import java.io.FileReader;
 import javax.swing.*;
 
-
 public class Panel extends JPanel {
+
     private final JPanel panel;
     private JTextArea editor;
     private JScrollPane scroll;
@@ -19,9 +19,46 @@ public class Panel extends JPanel {
     private final JMenuItem saveAs;
     private final JMenuItem open;
     private JFileChooser elegir;
-    private File ruta; 
+    private File ruta;
     private File rutaT;
+
     public Panel() {
 
+        BorderLayout layout = new BorderLayout(10, 10);//Se crea el borderLayout con cordenadas 10,10
+        archivo = new JMenu("Archivo");//Se agrega un botton tipo JMenu con en el nombre archivo al panel interno
+        setLayout(layout);
+        panel = new JPanel();
+        panel.setLayout(new GridLayout(1, 1));
+        add(panel, BorderLayout.CENTER);
+        menu = new JMenuBar();
+        save = new JMenuItem("Guardar");//Se agrega un botton tipo JMenu con en el nombre guardar al panel interno
+        save.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+//                save();
+            }
+        });
+        open = new JMenuItem("Abrir");//Se agrega un botton tipo JMenu con en el nombre abrir al panel interno
+        open.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+//                open();
+            }
+        });
+        saveAs = new JMenuItem("Guardar como");//Se agrega un botton tipo JMenu con en el nombre guardar como al panel interno
+        saveAs.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                rutaT = ruta;
+                ruta = null;
+//                save();
+            }
+        });
+        archivo.add(open);//Se agregan a la variable archivo "abrir"
+        archivo.add(save);//Se agregan a la variable archivo "guardar"
+        archivo.add(saveAs);//Se agregan a la variable archivo  "guardar como"
+        menu.add(archivo);//Todo lo que hay en la variable archivo se agrega al menu
+        add(menu, BorderLayout.NORTH);
     }
+
 }

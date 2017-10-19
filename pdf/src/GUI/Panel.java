82db50bc9c3d521b507a51472d740def150c3c2a
DESKTOP-9VS7B9R\santo <diegohans96@gmail.com>
2017-10-18 19:46:06 -0500

    Agregacion de metodo open y getContenido
    Se agrego el metodo para abrir el pdf y el metodo getContenido para obtener el contenido del mismo

--- pdf/src/GUI/Panel.java
+++ pdf/src/GUI/Panel.java
@@ -35,14 +35,14 @@ public class Panel extends JPanel {
         save.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                save();
+                save();//Llamada al metodo guardar
             }
         });
         open = new JMenuItem("Abrir");//Se agrega un botton tipo JMenu con en el nombre abrir al panel interno
         open.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                open();
+                open();//Llamada al metodo abrir
             }
         });
         saveAs = new JMenuItem("Guardar como");//Se agrega un botton tipo JMenu con en el nombre guardar como al panel interno
@@ -51,7 +51,7 @@ public class Panel extends JPanel {
             public void actionPerformed(ActionEvent e) {
                 rutaT = ruta;
                 ruta = null;
-                save();
+                save();//Llamada al metodo guardar 
             }
         });
         archivo.add(open);//Se agregan a la variable archivo "abrir"
@@ -61,10 +61,47 @@ public class Panel extends JPanel {
         add(menu, BorderLayout.NORTH);
     }
 
-    public void nuevo() {
-        editor = new JTextArea();
-        scroll = new JScrollPane(editor);
-        panel.add(scroll);
+    public void nuevo() {//Metodo para crear un nuevo pdf
+        editor = new JTextArea();//Se crea un JTextArea para poder escribir
+        scroll = new JScrollPane(editor);//Se crea el JScrollPane para habilitar el editor
+        panel.add(scroll);//Se agrega el JScrollPane al panel
+    }
+
+    public String open() {//Metodo para abrir un pdf
+        String nombre = "";
+        elegir = new JFileChooser();//Se crea el JFileChooser para poder hacer la seleccion de archivo a abrir
+        if (elegir.showOpenDialog(this) == JFileChooser.APPROVE_OPTION) {// Si apretamos en aceptar ocurrirá esto
+            try {
+                ruta = elegir.getSelectedFile();//Se guarda la ruta del archivo en la variable ruta despues de haber sido seleccionado
+                String texto = getContenido();//el contenido del pdf se guarda en texto
+                editor.setText(texto);//Editar el texto que contiene el pdf
+                nombre = elegir.getSelectedFile().getName();//Extrae el nombre del pdf
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog(null, "alert", "alert", JOptionPane.ERROR_MESSAGE);
+            }
+        }
+        return nombre;
+    }
+
+    public String getContenido() {
+        String contenido = "";
+        FileReader fr;
+        BufferedReader br = null;
+        try {
+            fr = new FileReader(ruta);
+            br = new BufferedReader(fr);
+            String linea;
+            while ((linea = br.readLine()) != null) {
+                contenido += linea + "\n";
+            }
+        } catch (Exception e) {
+        } finally {
+            try {
+                br.close();
+            } catch (Exception ex) {
+            }
+        }
+        return contenido;
     }
 
 }
